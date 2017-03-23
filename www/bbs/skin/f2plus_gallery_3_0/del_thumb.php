72067c5ccf90e71205c558b8c78997d56ce33f5c
YounChan Lee <blrun39@hanafos.com>
2017-03-23 20:06:32 +0900

    오픈소스 게시판 수정증보판 1.3.5.417 EUC-KR(한국어) 버전 배포...
    9. f2plus_gallery 2.0, 3.0 스킨에서 del_thumb.php 파일 공백 조정.
    ============================================================================
    
    bbs/skin/f2plus_gallery_2_0/del_thumb.php - 12줄 추가변경
    bbs/skin/f2plus_gallery_3_0/del_thumb.php - 12줄 추가변경
    
    이렇게 2개 파일에서 총 24줄이 추가 변경되었습니다. 이미 1.3.5.393 버전을
    다운로드 받으셨던 분들은 1.3.5.417 버전을 다시 다운로드 받아 해당 파일을
    교체하시기 바랍니다.
    
    source: https://www.blrun.net:47006/bbs/zboard.php?id=bug1&no=165#424

--- www/bbs/skin/f2plus_gallery_3_0/del_thumb.php
+++ www/bbs/skin/f2plus_gallery_3_0/del_thumb.php
@@ -3,18 +3,18 @@
 @extract($_POST); 
 
 function rmdirAll($dir) {
-   $dirs = dir($dir);
-   while(false !== ($entry = $dirs->read())) {
-      if(($entry != '.') && ($entry != '..')) {
-         if(is_dir($dir.'/'.$entry)) {
-            rmdirAll($dir.'/'.$entry);
-         } else {
-            @unlink($dir.'/'.$entry);
-         }
-       }
-    }
-    $dirs->close();
-    @rmdir($dir);
+	$dirs = dir($dir);
+	while(false !== ($entry = $dirs->read())) {
+		if(($entry != '.') && ($entry != '..')) {
+			if(is_dir($dir.'/'.$entry)) {
+				rmdirAll($dir.'/'.$entry);
+			} else {
+				@unlink($dir.'/'.$entry);
+			}
+		}
+	}
+	$dirs->close();
+	@rmdir($dir);
 }
 
 function delete_file($filename) {
