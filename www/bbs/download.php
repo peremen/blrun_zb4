be7f667d4fa7635cac1636c97038ea6dced7ba51
YounChan Lee <blrun39@hanafos.com>
2018-04-10 20:33:03 +0900

    오픈소스 게시판 수정증보판 1.4.1.748 UTF-8(다국어) 버전 배포...
    10. 한글 이름의 첨부파일을 다운로드 할 때 일부 모바일 브라우저 및
    리눅스 파이어팍스 브라우저에서 한글이 깨지는 문제점 수정.
    ============================================================================
    
    bbs/download.php - 21줄 추가변경
    bbs/download_c.php - 21줄 추가변경
    
    이렇게 2개 파일에서 총 42줄이 추가 변경되었습니다. 이미 1.4.1.706 버전을
    다운로드 받으셨던 분들은 1.4.1.748 버전을 다시 다운로드 받아 해당 파일을
    교체하시기 바랍니다.
    
    source: https://www.blrun.net/bbs/zboard.php?id=bug1&no=184#525

--- www/bbs/download.php
+++ www/bbs/download.php
@@ -25,12 +25,27 @@ if($filenum==1) {
 $data=mysql_fetch_array(mysql_query("select * from `$t_board"."_$id` where no='$no'"));
 
 // 다운로드;;
-$filename="s_file_name".$filenum;
-$filepath=$data["file_name".$filenum];
-$filename=$data[$filename];
-$filename=iconv("utf-8","euc-kr",$filename);
+function mb_basename($path) { return end(explode('/',$path)); }
+function utf2euc($str) { return iconv("UTF-8","cp949//IGNORE",$str); }
+function is_ie() {
+	if(!isset($_SERVER['HTTP_USER_AGENT'])) return false;
+	if(strpos($_SERVER['HTTP_USER_AGENT'],'MSIE')!==false) return true; // IE10 이하
+	if(strpos($_SERVER['HTTP_USER_AGENT'],'rv:11')!==false) return true; // IE11
+	if(strpos($_SERVER['HTTP_USER_AGENT'],'Edge')!==false) return true; // Edge
+	return false;
+}
 
-header("Content-Type: application/force-download");
+$filepath = $data["file_name".$filenum];
+$filesize = filesize($filepath);
+$filename = mb_basename($filepath);
+if(is_ie()) $filename = utf2euc($filename);
+
+header("Pragma: public");
+header("Expires: 0");
+header("Content-Type: application/octet-stream");
 header("Content-Disposition: attachment; filename=\"$filename\"");
-readfile("$filepath");
+header("Content-Transfer-Encoding: binary");
+header("Content-Length: $filesize");
+
+readfile($filepath);
 ?>
