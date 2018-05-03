4cbbd2fd6e7d3d7522beac241ba14192cc8b805f
YounChan Lee <blrun39@hanafos.com>
2018-05-03 09:49:36 +0900

    오픈소스 게시판 수정증보판 1.4.2.4 UTF-8(다국어) 버전 배포... (최종판 11)
    1. 포스트 제목에 & 문자 존재시 IE11에서 메인페이지의 rss 버튼을 클릭하면 에러가
    뜨면서 피드가 보이지 않는 문제점 해소. 크롬에서는 제대로 보이는데 rss2에서는
    크롬에서조차도 에러가 뜨면서 안보이기 때문에 이번에 버그 수정.  (단 > < 등과
    같은 꺾쇠는 글쓰기시 [html적용] 체크를 하면 브라우저 rss 피드에서 제대로
    보이지만 [html적용] 체크를 풀고 글을 등록하면 &gt; &lt; 등과 같은 문자로 보이니
    참고 바랍니다. 이건 html 태그로 등록하느냐 마느냐 하는 문제로 버그가 아닙니다.)
    
    첨부 파일 안의 3개의 스크립트 파일(0_DEL_BKSL.php, 1_ADD_COL.php,
    2_MOD_LVL.php)은 EUC-KR 용으로 제작된 스크립트라서 처음 UTF-8 버전을 설치하시는
    분들은 필요없으니 삭제하시고 EUC-KR에서 업그레이드 하시는 분들은 DB를 우선
    UTF-8로 업그레이드 한 다음 1.2.8.1656 (최종판4)에서 설명해 드린 내용을 우선 확인
    후 이 배포판을 사용하시기 바랍니다.
    
    ※ 현재 자동로그인을 사용해도 해커가 다른 아이피로 접속시 쿠키 탈취는 불가능하며
    동일 아이피 선상에서 쿠키 해킹을 방지하는 루틴은 이번 업데이트에 적용하지
    않았습니다. 이게 플래쉬 쿠키를 사용해야 하는 등 편법적인 방법이라서 약간
    까다로와 아직 적용하지 못했습니다.
    
    추가되거나 변경된 파일의 갯수는 모두 2개이고 변경된 총 코드 줄수는 4줄이며
    2018년 5월 3일 부터 수정된 파일만 교체하시면 됩니다.
    
    예전에 외부 html 소스에서 썸네일 추출할 때 게시판이 랙 걸리는 문제는 전격 해소가
    되었습니다.(갤러리 게시판 사용도중 랙이 걸려서 포스트를 수정해 줘야 하는
    문제점도 해소됨) ARM 보드의 SD카드 저장소를 사용한 웹서버에선 f2plus 2.0과 3.0
    게시판이 약간 느리게 동작할 수 있으니 하드나 SSD를 저장소로 사용하셔도 좋습니다.
    
    아래 파일 리스트에서 - 다음 숫자는 해당 파일에서 수정된 총 코드 줄 수이며 이
    코드 줄 수를 합치면 전체 수정 코드 줄 수가 산출됩니다.
    
    서버 구축방법은 http://bit.ly/1iyq6pJ 를 참고하세요. ^^;
    
    [변경되거나 추가된 파일] 2018-05-03 "www/rss/index.php" - 2 2018-05-03
    "www/rss/old_rss2/zero_rss.php" - 2
    
    source: https://www.blrun.net/bbs/zboard.php?id=bug1&no=188

--- www/rss/old_rss2/zero_rss.php
+++ www/rss/old_rss2/zero_rss.php
@@ -108,7 +108,7 @@ $result = mysql_query($query);
 while ($data_board = mysql_fetch_array($result))
 {
 $bbs_tmp[] = $bbss[$i];
-$subject[] = str_replace("\"","&quot;",$data_board[subject]);
+$subject[] = htmlspecialchars($data_board[subject]);
 $name[] = htmlspecialchars($data_board[name]);
 
 $category_name[] = htmlspecialchars($data_board[category_name]);
@@ -231,7 +231,7 @@ setlocale (LC_TIME,"ko");
 $name_sq = "<br /><br />작성자 : ".$name[$i]."<br />작성일자: ".strftime("%Y년 %m월 %d일 %A %p %I:%M:%S",$datetm[$i])."";
 ?>
 <item>
-<title><?=$name[$i]?> - <?=del_html(str_replace("\"","&quot;",$subject[$i]))?><?=$comments?></title>
+<title><?=$name[$i]?> - <?=$subject[$i]?><?=$comments?></title>
 <link><?=$target?>?id=<?=$bbs_tmp[$i]?>&amp;no=<?=$num[$i]?></link>
 <description><?=htmlspecialchars($memoss.$name_sq)?></description>
 <author><?=$name[$i]?></author>
